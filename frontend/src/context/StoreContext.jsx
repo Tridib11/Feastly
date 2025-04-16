3b8d7190df7b95ac677e84ccc3df27b9e6246a58
Tridib11 <tridibghosh12345@gmail.com>
2025-04-17 00:06:57 +0530

    Refactor FoodItem component to utilize cartItems state for item count management and update add/remove functionality

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,10 +1,12 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
   const[cartItems,setCartItems]=useState({})
+  
+
   const addToCart=(itemId)=>{
     if(!cartItems[itemId]){
       setCartItems((prev)=>({...prev,[itemId]:1}))
